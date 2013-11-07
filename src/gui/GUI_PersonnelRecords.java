2ee731df82e2c39418784cb2852e649e04fc6362
nikitenko.roman@gmail.com <nikitenko.roman@gmail.com>
2013-11-07 15:23:24 +0200

    Added buttons Add and Delete

--- src/gui/GUI_PersonnelRecords.java
+++ src/gui/GUI_PersonnelRecords.java
@@ -2,7 +2,9 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,13 +12,19 @@ import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.UIManager;
+import javax.swing.border.TitledBorder;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableModel;
+import javax.xml.bind.annotation.XmlType.DEFAULT;
 
 import test.Test;
 import util.MyUtil;
@@ -35,13 +43,16 @@ public class GUI_PersonnelRecords extends JFrame implements ActionListener {
 		
 		new MyMenu();
 		
-		//генерируем random-data по сотрудникам
+		//генерируем random-data по сотрудникам в файл "TestListEmployee.out"
 		Test.generationEmployeeDataAndFiling(100, "TestListEmployee.out");
 		
 		//считываем данные по сотрудникам из файла
 		arrListDataEmployees = MyUtil.readDataEmployeeFromFile("TestListEmployee.out");
 		
-		//создаем таблицу на основе нашей модели
+		//получаем размер экрана
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		
+//*******//создаем таблицу на основе нашей модели
 		JTable dataTable = new JTable(new MyTableModel());
 		dataTable.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		
@@ -49,7 +60,7 @@ public class GUI_PersonnelRecords extends JFrame implements ActionListener {
         JScrollPane jscrlp = new JScrollPane(dataTable, ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
         
         //”станаливаем размеры прокручиваемой области
-//        dataTable.setPreferredScrollableViewportSize(new Dimension(250, 100));
+//        dataTable.setPreferredScrollableViewportSize(new Dimension(screenSize.width-50, screenSize.height-50));
         //«адаем размер столбцов
         dataTable.getColumnModel().getColumn(0).setPreferredWidth(55);
         dataTable.getColumnModel().getColumn(1).setPreferredWidth(200);
@@ -60,44 +71,55 @@ public class GUI_PersonnelRecords extends JFrame implements ActionListener {
         dataTable.getColumnModel().getColumn(6).setPreferredWidth(100);
         dataTable.getColumnModel().getColumn(7).setPreferredWidth(220);
         dataTable.getColumnModel().getColumn(8).setPreferredWidth(300);
+        
 //*******//главна€ панель//*******
-		JPanel mainPanel = new JPanel(); //создаем главную панель, задаем менеджер размещени€
-		mainPanel.setPreferredSize(new Dimension(900, 410));//устанавливаем размер
+        
+        JPanel mainPanel = new JPanel(); //создаем главную панель, задаем менеджер размещени€
+		mainPanel.setPreferredSize(new Dimension(screenSize.width, screenSize.height-25));//устанавливаем размер
 		mainPanel.setBorder(BorderFactory.createRaisedBevelBorder());//устанавливаем бордюр
 		
+//*******//настройки окна//*******
 		add(mainPanel);//добавл€ем главную панель к фрейму
 		this.setJMenuBar(MyMenu.menuBar);//устанавливаем меню
 		setTitle("PersonnelRecord"); //устанавливаем заголовок окна
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);//завершение работы приложени€ по закрытию главного окна
 //		setLocationRelativeTo(null); //расположение окна - левый верхний угол по центру экрана
-		setSize(1200, 650); // задаем размеры окна
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		setSize(new Dimension(screenSize.width, screenSize.height-25));
 		setLocation((screenSize.width - getWidth()) / 2,
 		                (screenSize.height - getHeight()) / 3);
 		setVisible(true); // отображать окно
-//		setExtendedState(MAXIMIZED_BOTH);//окно на весь экран
+		setExtendedState(MAXIMIZED_BOTH);//окно на весь экран
 		
 //*******//ѕанель Employee with fixed salary//*******
 		JPanel panelEmployeeFixSalary = new JPanel(new BorderLayout());
 		panelEmployeeFixSalary.setPreferredSize(new Dimension (mainPanel.getWidth()-10, mainPanel.getHeight()-10));
-		panelEmployeeFixSalary.setBorder(BorderFactory.createTitledBorder(BorderFactory.createRaisedBevelBorder(), "Employee With Fix Salary"));
+		panelEmployeeFixSalary.setBorder(BorderFactory.createTitledBorder(BorderFactory.createRaisedBevelBorder(), "Employee With Fix Salary",
+				TitledBorder.CENTER, TitledBorder.TOP));
 		
-		//панель дл€ кнопок EDIT
-		JPanel panelButEdit = new JPanel();
-		panelButEdit.setLayout(new BoxLayout(panelButEdit, BoxLayout.Y_AXIS));
+//*******//панель дл€ кнопок EDIT
+		JPanel panelButEdit = new JPanel(new GridLayout(7, 1, 0, 10));
+//		panelButEdit.setLayout(new BoxLayout(panelButEdit, BoxLayout.Y_AXIS));
 		panelButEdit.setPreferredSize(new Dimension (100, mainPanel.getHeight()-10));
 		panelButEdit.setBorder(BorderFactory.createTitledBorder(BorderFactory.createRaisedBevelBorder(), "Buttons Edit"));
 		panelEmployeeFixSalary.add(panelButEdit, BorderLayout.WEST);
 		
-		//панель дл€ таблицы
+//*******//кнопоки EDIT
+		JButton butAdd = new JButton("Add");
+		panelButEdit.add(butAdd);
+		
+		JButton butDelete = new JButton("Delete");
+		panelButEdit.add(butDelete);
+		
+//*******//панель дл€ таблицы
 		JPanel panelTable = new JPanel(new BorderLayout());
 		panelTable.setPreferredSize(new Dimension (mainPanel.getWidth()-panelButEdit.getWidth()-10, mainPanel.getHeight()-10));
 		panelTable.setBorder(BorderFactory.createTitledBorder(BorderFactory.createRaisedBevelBorder(), "Table Data"));
 		
-		//ƒобавл€ем в на панель прокрути jscrlp вместе с таблицей
+		//ƒобавл€ем на панель jscrlp вместе с таблицей
 		panelTable.add(jscrlp);
+		
+		//¬кладываем панели
 		panelEmployeeFixSalary.add(panelTable,BorderLayout.CENTER);
-
 		mainPanel.add(panelEmployeeFixSalary, BorderLayout.CENTER);
 		
 		
