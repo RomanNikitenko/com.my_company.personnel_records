f1abb2dba767d3dc1c690c042d2c2fa45d632735
Roman Nikitenko <rnikitenko@codenvy.com>
2015-06-06 12:46:13 +0300

    ddd

--- src/main/java/com/company/personnelrecords/company/Company.java
+++ src/main/java/com/company/personnelrecords/company/Company.java
@@ -23,7 +23,7 @@ public class Company {
 	long 	companyCurrentAccount,
 			companyEDRPOU;
 
-	//*******constructor*********444444*************************
+	//*******constructor**********************************
 	private Company(String companyName, String companyCEO, 
 			long companyCurrentAccount, long companyEDRPOU, String companyRegisteredOffice){
 		this.companyName = companyName;
